be7f324ed0553c9efa5a427e9519e3e1573c42fa
matemat13 <shrajtofle@gmail.com>
2015-03-24 22:03:44 +0100

    Přidán soubor main.m
    Hlavní skript projektu

---
+++ main.m
@@ -0,0 +1,56 @@
+%%Definice konstant a promennych
+syms k1 k2 k3 phim phip phimd phipd M kmot delta b Jm Jp m r l g;
+
+%Substituovane konstanty
+k1 = (Jm + m*r^2);
+k2 = (m*l*r);
+k3 = (m*g*l);
+
+%%Stavovy popis
+%Definice stavu
+x1 = phim;
+x2 = phip;
+x3 = phimd;
+x4 = phipd;
+xs = [x1; x2; x3; x4];
+
+u1 = M;
+us = u1;
+
+%Stavove rovnice
+x1d = x3;
+x2d = x4;
+x3d = (-Jp*k2*cos(x2)*x4^2 + 2*delta*k2*sin(x2)*x4 - Jp*M + Jp*b*x3 + k2*k3*cos(x2)*sin(x2))/(-Jp*k1 + k2^2*sin(x2));
+x4d = (2*delta*x3 + k3*cos(x2) - k2^2/k1*sin(x2)*cos(x2)*x4 - k2/k1*sin(x2)*M + k2*b/k1*sin(x2)*x4)/(-Jp+k2^2/k1*sin(x2)^2);
+xds = [x1d x2d x3d x4d];
+
+y1 = x1;
+y2 = x2;
+ys = [y1 y2];
+
+%%Linearizace
+%Pracovni bod
+y1p = 0;
+y2p = 0;
+[x1p, x2p, x3p, x4p, up1, params, conds] = solve([x1d == 0, x2d == 0, x3d == 0, x4d == 0, y1 == y1p, y2 == y2p], [x1, x2, x3, x4, u1], 'ReturnConditions', true);
+xps = [x1p; x2p; x3p; x4p];
+ups = up1;
+
+%Jakobiany stavovych rovnic
+Alin = jacobian(xds, xs);
+Blin = jacobian(xds, u1);
+Clin = jacobian(ys, xs);
+Dlin = jacobian(ys, u1);
+
+%Zadane parametry
+m = 0.175;
+g = 9.81;
+
+%Zjistene parametry
+%zatim nic :(
+
+%Dosazeni pracovniho bodu
+Alin = subs(Alin, xs, xps);
+Blin = subs(Blin, us, ups);
+Clin = subs(Clin, xs, xps);
+Dlin = subs(Dlin, us, ups);
