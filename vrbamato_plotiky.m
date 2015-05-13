e19647b0756cee0d063f601731847777d5fff131
Tomáš Glabazňa <glabazna.t@gmail.com>
2015-05-13 13:39:01 +0200

    Zbytecna zmena, kterou si Matouš musí teď pullnout, aby nebyl konflikt.

--- vrbamato_plotiky.m
+++ vrbamato_plotiky.m
@@ -1,34 +1,34 @@
 %%
-%Kyvadlo - odezva na poruchu s regulátorem poly
-close all;
+% %Kyvadlo - odezva na poruchu s regulï¿½torem poly
+% close all;
+% 
+% %%
+% %Odezva polohy
+% figure;
+% t_odezva = kyvadlo_odezva_porucha.time;
+% sig_odezva = kyvadlo_odezva_porucha.signals.values(:,2);
+% plot(t_odezva, sig_odezva, 'b', 'LineWidth', 2);
+% grid on;
+% hold on;
+% xlim([t_odezva(1), t_odezva(end)]);
+% xlabel('ï¿½as [s]');
+% ylabel('Poloha kyvadla [rad]');
+% plot([t_odezva(1) t_odezva(end)], -pi/2*ones(2), '--', 'color', [0.1 0.1 0.1]);
+% legend('Odezva kyvadla na poruchu', 'Referenï¿½nï¿½ poloha');
+% 
+% %%
+% %Akï¿½nï¿½ zï¿½sah
+% figure;
+% t_odezva = kyvadlo_odezva_porucha_akcnizasah(:,1);
+% sig_odezva = kyvadlo_odezva_porucha_akcnizasah(:,2);
+% plot(t_odezva, sig_odezva, 'r', 'LineWidth', 2);
+% grid on;
+% xlim([t_odezva(1), t_odezva(end)]);
+% xlabel('ï¿½as [s]');
+% ylabel('Hodnota akï¿½nï¿½ho zï¿½sahu [-]');
 
 %%
-%Odezva polohy
-figure;
-t_odezva = kyvadlo_odezva_porucha.time;
-sig_odezva = kyvadlo_odezva_porucha.signals.values(:,2);
-plot(t_odezva, sig_odezva, 'b', 'LineWidth', 2);
-grid on;
-hold on;
-xlim([t_odezva(1), t_odezva(end)]);
-xlabel('Èas [s]');
-ylabel('Poloha kyvadla [rad]');
-plot([t_odezva(1) t_odezva(end)], -pi/2*ones(2), '--', 'color', [0.1 0.1 0.1]);
-legend('Odezva kyvadla na poruchu', 'Referenèní poloha');
-
-%%
-%Akèní zásah
-figure;
-t_odezva = kyvadlo_odezva_porucha_akcnizasah(:,1);
-sig_odezva = kyvadlo_odezva_porucha_akcnizasah(:,2);
-plot(t_odezva, sig_odezva, 'r', 'LineWidth', 2);
-grid on;
-xlim([t_odezva(1), t_odezva(end)]);
-xlabel('Èas [s]');
-ylabel('Hodnota akèního zásahu [-]');
-
-%%
-%Kyvadlo - odezva na poruchu s regulátorem autotune
+%Kyvadlo - odezva na poruchu s regulï¿½torem autotune
 close all;
 
 %%
@@ -41,17 +41,17 @@ hold on;
 plot(t_odezva, sig_odezva, 'b', 'LineWidth', 2);
 grid on;
 xlim([t_odezva(1), t_odezva(end)]); 
-xlabel('Èas [s]');
+xlabel('ï¿½as [s]');
 ylabel('Poloha kyvadla [rad]');
-legend('Referenèní poloha', 'Odezva kyvadla na poruchu');
+legend('Referenï¿½nï¿½ poloha', 'Odezva kyvadla na poruchu');
 
 %%
-%Akèní zásah
+%Akï¿½nï¿½ zï¿½sah
 figure;
 t_odezva = kyvadlo_odezva_pocpodm_PID_akcnizasah.time;
 sig_odezva = kyvadlo_odezva_pocpodm_PID_akcnizasah.signals.values(:,1);
 plot(t_odezva, sig_odezva, 'r', 'LineWidth', 2);
 grid on;
 xlim([t_odezva(1), t_odezva(end)]);
-xlabel('Èas [s]');
-ylabel('Hodnota akèního zásahu [-]');
+xlabel('ï¿½as [s]');
+ylabel('Hodnota akï¿½nï¿½ho zï¿½sahu [-]');
